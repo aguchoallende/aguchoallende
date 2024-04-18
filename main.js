05bbf09a86c13ff67d7cb5630058677d4d15ce82
Manzano <162187492+aguchoallende@users.noreply.github.com>
2024-04-18 20:13:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
-(function ($) {
-    "use strict";
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: true,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
-})(jQuery);
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
+const images = document.querySelectorAll('redessociales');
+function triggerAnimation(entries){
+    entries.forEach(entry =>{
+        const image = entry.target.querySelector('img');
 
+        image.classList.toggle('unset', entry,isIntersecting);
+    })
+}
+const options ={
+    root: null,
+    rootMargin: "0px",
+    threshold: 1
+}
+
+const observer = new IntersectionObserver(triggerAnimation, options);
+
+images.forEach(image =>{
+    observer.observe(image);
+})
